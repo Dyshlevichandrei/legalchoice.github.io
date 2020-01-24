9b026b5c15d1f571fcd694bfd11a0aae3c778587
Andrei <45993907+Dyshlevichandrei@users.noreply.github.com>
2020-01-24 13:54:24 +0300

    Add files via upload

---
+++ core/send.php
@@ -0,0 +1,26 @@
+﻿<?php
+ 
+   $to = 'korzhova_alesya@mail.ru, dyshlevichandrei@gmail.com';       
+   $from = 'info@dreamvacation.by'; 
+   $subject = 'Заказ с сайта dreamvacation'; 
+   $subject = '=?utf-8?b?'. base64_encode($subject) .'?=';
+   $headers = "Content-type: text/plain; charset=\"utf-8\"\r\n"; 
+   $headers .= "From: <". $from .">\r\n"; 
+   $headers .= "MIME-Version: 1.0\r\n"; 
+   $headers .= "Date: ". date('D, d M Y h:i:s O') ."\r\n"; 
+   $message = 'хочу заказывать у вас тур'; 
+if(isset($_POST['name']))
+
+$message = "
+Информация о покупателе:
+
+Имя: ".$_POST['name']."
+E-mail: ".$_POST['e-mail']."
+Телефон: ".$_POST['phone']."
+Время заказа: ".date("Y-m-d H:i:s");
+   
+   mail($to, $subject, $message, $headers);
+
+{
+	header('Location: good.html');
+}
